16997a604bfab8cce0a47c986dc0882b7852f310
charles <charles@hellomarketapp.com>
2015-01-11 17:43:14 +0900

    added mongoose for using mongodb

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-var express = require('express');
+// Mongo Connection URL
+var mongoUrl = 'mongodb://localhost:27017/pagas';
+
 var path = require('path');
 var http = require('http');
+var express = require('express');
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    autoIncrement = require('mongoose-auto-increment');
+
+mongoose.connect(mongoUrl);
+
+autoIncrement.initialize(mongoose);
+
+// Define Mongo Model
+var postSchema = new Schema({
+  'text': String,
+  'date': { type: Date, default: Date.now }
+});
+
+postSchema.plugin(autoIncrement.plugin, 'Post');
+var Post = mongoose.model('post', postSchema);
+
 var app = express();
-var mongo = require('./mongo');
 
 // Set Express Environments
 app.set('port', process.env.PORT||1337);
@@ -15,9 +34,27 @@ http.createServer(app).listen(app.get('port'), function() {
   console.log("Express server listening!!");
 });
 
-// Route Url
+// Show Post List Page
 app.get('/', function(req, res) {
-  mongo.find('testData', {}, function(docs) {
-    res.send(docs);
+
+  var post = new Post();
+  post.text = 'a';
+  post.save(function(err){
+    if(err) console.log("Something went wrong while saving the thing");
+    else console.log("Thing was successfully saved");
   });
+  res.send('ok');
+});
+
+// Show Post Detail Page
+app.get('/post/:id', function(req, res) {
+});
+
+// Show Add Post Page
+app.get('/post/add', function(req, res) {
+  res.send('Show Add Post');
+});
+
+// Do Add Post
+app.post('/post/add', function(req, res) {
 });
